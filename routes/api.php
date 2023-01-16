697ca4d01536f81e1a03f9a48df31b1203609094
josecaseiro <jvcaseiro@hotmail.com>
2023-01-16 13:14:56 +0100

    spaces ajusts

--- routes/api.php
+++ routes/api.php
@@ -22,27 +22,27 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('musicas',function(){
+Route::get('musicas', function () {
     $musicas = Musica::all();
     return compact('musicas');
 });
-Route::get('musicas/{nome}',function($nome){
-    $musicas = Musica::where('titulo','like','%'.$nome.'%')->get();
+Route::get('musicas/{nome}', function ($nome) {
+    $musicas = Musica::where('titulo', 'like', '%' . $nome . '%')->get();
     return compact('musicas');
 });
-Route::get('musicas/destaque',function(){
-    $musicas = Musica::orderBy('id','DESC')->limit(6)->get();
+Route::get('musica/destaque', function () {
+    $musicas = Musica::orderBy('id', 'DESC')->limit(6)->get();
     return compact('musicas');
 });
-Route::get('artistas',function(){
+Route::get('artistas', function () {
     $artista = Artista::all();
     return compact('artista');
 });
-Route::get('videos',function(){
+Route::get('videos', function () {
     $videos = Video::all();
     return compact('videos');
 });
-Route::get('galeria',function(){
+Route::get('galeria', function () {
     $img = Imagem::all();
     return compact('img');
 });
